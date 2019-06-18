a65e041193cc093ec0a0dcaed9fafdbb8c19b2fc
justiny2c <justiny2c@gmail.com>
2019-06-18 14:11:47 -0700

    Add Todo button working

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -36,46 +36,18 @@ class TodoList extends Component {
                     value={this.state.newTodo}
                     placeholder="More things todo..."
                     onChange={this.handleChange}/>
-                <button onClick={() => {this.addTodo() }}>Add</button>
+                <button onClick={this.addTodo}>Add</button>
 
-                {/* <input
-                    placeholder="More things todo..."></input>
-                <button></button>
-                <button onClick={() => {this.props.increment() }}>
-                    +
-                </button>
-                <button onClick={() => {this.props.decrement() }}>
-                    -
-                </button>
-                 {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-                {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
             </div>
         );
     }
 }
 
-// The mapStateToProps function specifies which portion of the
-// state tree this component needs to receive. In this case,
-// since our redux store is only storing the value of the count,
-// this component receives the whole state. In a more complex
-// redux application, though, it would receive only the relevant
-// parts it needs from the state object.
 const mapStateToProps = (state) => {
-    // console.log(state)
+
     return {
         todos: state.todos
     };
 };
 
-// The connect function is called in order to make this component aware
-// of the rest of the redux architecture. Without this, this component
-// is only a dumb React component. We pass in all of the functions that
-// are reliant on Redux, along with the component itself, so that Redux
-// makes itself known to this component.
 export default connect(mapStateToProps, { addTodo })(TodoList);
