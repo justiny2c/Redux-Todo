86d48a44adefb7305b9cff79646bee89b538e185
justiny2c <justiny2c@gmail.com>
2019-06-18 13:04:33 -0700

    Initial push

---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions';
+
+class TodoList extends Component {
+    // incrementIfOdd = () => {
+    //     // Stretch Problem: Implement an increment function that
+    //     // only increments if the counter value is odd
+    //     if(this.props.count % 2 === 1){
+    //         this.props.increment()
+    //     }
+    // };
+    render() {
+
+        return (
+            <p>
+                {this.props.todoList}
+                <input
+                    placeholder="More things todo..."></input>
+                <button></button>
+                <button onClick={() => {this.props.increment() }}>
+                    +
+                </button>
+                <button onClick={() => {this.props.decrement() }}>
+                    -
+                </button>
+                 {/* Uncomment these button tags if you got
+                around to implementing the extra credit functions */}
+                <button onClick={this.incrementIfOdd}>
+                    Increment if odd
+                </button>
+                <button onClick={this.incrementAsync}>
+                    Increment async
+                </button> 
+            </p>
+        );
+    }
+}
+
+// The mapStateToProps function specifies which portion of the
+// state tree this component needs to receive. In this case,
+// since our redux store is only storing the value of the count,
+// this component receives the whole state. In a more complex
+// redux application, though, it would receive only the relevant
+// parts it needs from the state object.
+const mapStateToProps = (state) => {
+    return {
+        count: state.count,
+        // example: state.example,
+    };
+};
+
+// The connect function is called in order to make this component aware
+// of the rest of the redux architecture. Without this, this component
+// is only a dumb React component. We pass in all of the functions that
+// are reliant on Redux, along with the component itself, so that Redux
+// makes itself known to this component.
+export default connect(mapStateToProps, { increment, decrement })(Counter);
